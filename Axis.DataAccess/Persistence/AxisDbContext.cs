4d0ba309a663dcba7452c840c5063adef0c6011b
unknown <nijumkamrul447@gmail.com>
2024-12-30 23:59:40 +0600

    add dbset

--- Axis.DataAccess/Persistence/AxisDbContext.cs
+++ Axis.DataAccess/Persistence/AxisDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Axis.Core.Models.HR;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,16 @@ namespace Axis.DataAccess.Persistence
         {
             
         }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeAddress> EmployeeAddresses { get; set; }
+        public DbSet<JobInformation> JobInformations { get; set; }
+        public DbSet<EmployeeFile> EmployeeFiles { get; set; }
+        public DbSet<EmpTaxInfo> EmpTaxInfos { get; set; }
+        public DbSet<FamilyInfo> FamilyInfos { get; set; }
+        public DbSet<WorkExperience> WorkExperiences { get; set; }
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<BankInfo> BankInfo { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
