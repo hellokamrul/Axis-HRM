821aa8c712c1c11dcdadd5e92241f7c57dd294b7
unknown <nijumkamrul447@gmail.com>
2025-02-21 05:26:58 +0600

    Adding Housekeeping DTO

---
+++ Axis.Application/DTOs/HouseKeeping/DepartmentDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axis.Application.DTOs.HouseKeeping
+{
+    public class DepartmentDTO : BaseDTO
+    {
+       
+        public string DeptCode { get; set; }
+        public string DeptName { get; set; }
+        public string DeptLocalName { get; set; }
+        public short? slno { get; set; }
+    }
+}
---
+++ Axis.Application/DTOs/HouseKeeping/SectionDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axis.Application.DTOs.HouseKeeping
+{
+    public class SectionDTO : BaseDTO
+    {
+        public string SectName { get; set; }       
+        public string SectLocalName { get; set; }     
+        public int? slno { get; set; }
+    }
+}
